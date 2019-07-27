928deadd5daec9b50b4e40fac59c917bdd4a0ab0
james0248 <hsjames0248@gmail.com>
2019-07-28 02:47:51 +0900

    Change data control

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
 
 const spawn = require("child_process").spawn;
-const process = spawn("python", ["./dust_chk.py"]);
+const process = spawn("python3", ["./dust_chk.py"]);
 
 app.set('port', 8000);
 
 process.stdout.on("data", data => {
-    const dustData = new Buffer(3);
-    for(i = 0; i < 3; i++){
-        dustData[i] = data[i];
-    }
-    process.emit("dust", dustData);
+    process.emit("dust", data);
 });
 
 app.get("/", (req, res) => {
@@ -23,7 +19,7 @@ app.get("/", (req, res) => {
 
 io.on("connection", socket => {
     console.log("io connected!");
-    process.on("dust", data => socket.emit("data", data));
+    process.on("dust", data => socket.emit("data", data.toString()));
 
 })
 
